6d5a9c103c3b698570cf09803f975cd93d6a61c7
AweSome <kasbek228@gmail.com>
2021-03-01 13:35:46 +0200

    Create ОПИ25.01.cpp

---
+++ ОПИ25.01.cpp
@@ -0,0 +1,11 @@
+﻿double WhatIsThis(double a[10])
+{
+	int i;
+	double prod = 0; 
+	double twoprod = 0;
+	for (int i = 0; i < 10; i++)
+		prod *= a[i];
+	for (int i = 0; i < 10; i++)
+		twoprod = prod*2;
+	return twoprod;
+}
